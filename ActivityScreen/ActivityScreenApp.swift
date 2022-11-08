84c00bd28577d3fc4de4968710b6e7f34698f7cf
zahrahalharbi <zaalharbi22@twq.idserve.net>
2022-11-08 08:43:11 +0300

    Initial Commit

---
+++ ActivityScreen/ActivityScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityScreenApp.swift
+//  ActivityScreen
+//
+//  Created by Zahrah. on 31/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct ActivityScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView1()
+        }
+    }
+}
